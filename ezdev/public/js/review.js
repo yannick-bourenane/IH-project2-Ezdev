c5da2ec69c5ed0bb0cca820b24733b7e645942de
Pierre Etryhard <pierre.etryhard.pro@gmail.com>
2020-02-12 14:35:20 +0100

    added stars and input to upload averageRate

--- ezdev/public/js/review.js
+++ ezdev/public/js/review.js
@@ -1,6 +1,9 @@
 const rates = document.querySelectorAll(".box-rate-review > .rating");
-rates.forEach(rate => displayStars(rate))
+// rates.forEach(rate => displayStars(rate));
 
+const stars = document.querySelectorAll(".star")
+const btnReview = document.getElementById("btn-review");
+const input = document.getElementById("rate-teacher");
 
 function countAverage() {
     let sum = 0;
@@ -11,18 +14,19 @@ function countAverage() {
     return avgRate = sum / rates.length;
 }
 
-function displayAverage() {
-    
+function colorStar() {
+    stars.forEach((star,i) => {
+        star.onclick = () => {
+            // star.classList.toggle("star-selected")
+            for(let j = 0; j <= i; j++){
+                stars[j].classList.toggle("star-selected")
+            }
+            input.value = star.getAttribute("data-type-rate");
+        }
+    })  
 }
 
 
-
-function displayStars(rate) {
-    const rateNumber = rate.getAttribute("data-type-rate");
-    rate.innerHTML = ``;
-    for (let i=0; i<rateNumber; i++) {
-        rate.innerHTML += `<i class="fas fa-star" style="color: orange"></i>`
-    }
-}
-
+colorStar();
 countAverage();
+addValueToName();
