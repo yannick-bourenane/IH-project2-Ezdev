f428c16373936b93c36c55a12fa43a3cd90d4e65
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-02-14 11:29:22 +0100

    herok²

--- ezdev/public/js/APIHandler.js
+++ ezdev/public/js/APIHandler.js
@@ -1,7 +1,7 @@
 // AXIOS SETUP
 const service = axios.create({
     //baseURL: 'http://localhost:9000'
-    baseURL: 'http://ezdev.herokuapp.com'
+    baseURL: 'https://ezdev.herokuapp.com'
 });
 
 export default service;
